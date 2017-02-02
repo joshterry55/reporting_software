4c9ee86dc35897bfd5434517cdc10c9608dd36bc
Josh Terry <joshterry55@gmail.com>
2017-02-02 10:49:25 -0700

    create company on refresh is fixed

--- client/components/CreateCompany.js
+++ client/components/CreateCompany.js
@@ -8,6 +8,10 @@ class CreateCompany extends React.Component {
     this.createCompany = this.createCompany.bind(this)
   }
 
+  componentDidMount() {
+    this.props.dispatch({type: 'RESET_ASSIGNED_COMPANY'})
+  }
+
   createCompany(e) {
     e.preventDefault()
     let name = this.refs.companyName.value
