1b5c4697f2606072e9185d349db4215677052582
Josh Terry <joshterry55@gmail.com>
2017-02-16 10:39:21 -0700

    videos stay sorted on edit, adding, and initial page load

--- client/reducers/trainingvideos.js
+++ client/reducers/trainingvideos.js
@@ -4,14 +4,28 @@ const trainingvideos = (state = [], action) => {
     case 'TRAINING_VIDEOS':
       return action.sortedVideos
     case 'ADD_TRAINING_VIDEO':
-      return [...state, action.video]
+      let added = [...state, action.video]
+      let sortedAddedVideos = added.sort(function(a, b){
+        if(a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+        if(a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+        return 0;
+      })
+      return sortedAddedVideos
+
     case 'UPDATE_TRAINING_VIDEO':
       let indexUpdate = state.findIndex( s => s.id === action.video.id)
-      return [
+      let sliced = [
         ...state.slice(0, indexUpdate),
         ...state.slice(indexUpdate + 1),
         action.video
       ]
+      let sortedVideos = sliced.sort(function(a, b){
+        if(a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+        if(a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+        return 0;
+      })
+      return sortedVideos
+
     case 'REMOVE_TRAINING_VIDEO':
     let index = state.findIndex( s => s.id === action.video.id)
       return [
