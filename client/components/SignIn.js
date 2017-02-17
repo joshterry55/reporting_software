872834267d1529d520def772256552dacd9c2bcf
Josh Terry <joshterry55@gmail.com>
2017-02-17 14:52:35 -0700

    sign in page change

--- client/components/SignIn.js
+++ client/components/SignIn.js
@@ -40,13 +40,29 @@ class SignIn extends React.Component {
   render() {
     return(
       <div className='row container'>
-        <div className='col s12'>
-          <h3 className='center' style={{color: '#f2f7f7'}}>Sign In</h3>
-          <form className='col s10 offset-s1 m8 offset-m2' onSubmit={this.handleSubmit}>
-            <input type="email" style={{color: '#f2f7f7'}} required={true} ref='email' placeholder='email' />
-            <input type='password' style={{color: '#f2f7f7'}} required={true} ref='password' placeholder='password' />
-            <button className='btn' style={{backgroundColor: '#60b9e8'}}>Sign In</button>
-          </form>
+        <div className='col s12' style={{paddingTop: '75px'}}>
+          <h3 className='center' style={{color: '#f2f7f7'}}>Login</h3>
+          <div className='col s12 m6 offset-m3 l4 offset-l4' style={{backgroundColor: '#ccc', padding: '20px', borderRadius: '5px'}}>
+            <div className='center' style={{position: 'relative', height: '105px'}}>
+              <div style={{
+                  height: '100px',
+                  width: '100px',
+                  borderRadius: '50%',
+                  display: 'inline-block',
+                  boxShadow: '0 0 2px rgba(0,0,0,0.35)',
+                  border: '1px solid #000',
+                  backgroundImage: `url('http://res.cloudinary.com/dk2bj79p0/image/upload/v1483585049/anonBee_wgbcih.jpg')`,
+                  backgroundSize: 'cover'
+                }}></div>
+            </div>
+            <form className='col s12' onSubmit={this.handleSubmit}>
+              <label style={{color: 'black'}}>Email</label>
+              <input type="email" style={{color: 'black', backgroundColor: '#f2f7f7', borderRadius: '2px'}} required={true} ref='email' placeholder='email' />
+              <label style={{color: 'black'}}>Password</label>
+              <input type='password' style={{color: 'black', backgroundColor: '#f2f7f7', borderRadius: '2px'}} required={true} ref='password' placeholder='password' />
+              <button className='btn' style={{backgroundColor: '#60b9e8'}}>Sign In</button>
+            </form>
+          </div>
         </div>
       </div>
     )
