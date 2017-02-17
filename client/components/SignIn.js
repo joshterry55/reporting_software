3214300b74fcd6b29456f36ec7e44629d3e01d91
Josh Terry <joshterry55@gmail.com>
2017-02-17 15:15:11 -0700

    logo above sign in

--- client/components/SignIn.js
+++ client/components/SignIn.js
@@ -41,7 +41,7 @@ class SignIn extends React.Component {
     return(
       <div className='row container'>
         <div className='col s12' style={{paddingTop: '75px'}}>
-          <h3 className='center' style={{color: '#f2f7f7'}}>Login</h3>
+          <h3 className='center' style={{color: '#f2f7f7'}}>Logo</h3>
           <div className='col s12 m6 offset-m3 l4 offset-l4' style={{backgroundColor: 'rgba(200,200,200,0.65)', padding: '20px', borderRadius: '5px'}}>
             <div className='center' style={{position: 'relative', height: '105px'}}>
               <div style={{
