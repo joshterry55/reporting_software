67c15ef91a9c1453169489855bbe529514d34539
Josh Terry <joshterry55@gmail.com>
2017-02-23 17:39:34 -0700

    ransack

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,6 +1,6 @@
-ransack.configure do |config|
-  config.add_predicate 'string_like_any',
-     arel_predicate: 'matches',
-     formatter: proc { |v| "%#{v.split(' ').join('%%')}%"},
-     validator: proc { |v| v.present? }
-end
+# ransack.configure do |config|
+#   config.add_predicate 'string_like_any',
+#      arel_predicate: 'matches',
+#      formatter: proc { |v| "%#{v.split(' ').join('%%')}%"},
+#      validator: proc { |v| v.present? }
+# end
