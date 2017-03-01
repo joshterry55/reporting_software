996fe2862cb38081cfea6c99bb46957787f30565
Josh Terry <joshterry55@gmail.com>
2017-02-28 20:05:51 -0700

    cancel

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module ReportingSoftware
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.time_zone = 'Mountain Time (US & Canada)'
-    config.active_record.default_timezone = :local
+
   end
 end
