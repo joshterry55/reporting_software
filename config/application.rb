f03638e59d7f16ed93c43aec7683a9794f55767d
Josh Terry <joshterry55@gmail.com>
2017-02-28 19:51:32 -0700

    heroku test

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.active_record.default_timezone = :local
+
 module ReportingSoftware
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
