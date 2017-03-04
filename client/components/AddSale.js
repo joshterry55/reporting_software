3fc2328cc097f717ac182180049af1d46ff8ce69
Josh Terry <joshterry55@gmail.com>
2017-03-04 12:05:39 -0700

    checkbox change

--- client/components/AddSale.js
+++ client/components/AddSale.js
@@ -134,11 +134,13 @@ employeeFind(id) {
       return(
         <form ref='saleForm' style={{padding: '15px'}} onSubmit={this.submitSale}>
           <div>
-            <label>Select a salesman</label>
-            <select ref='employee' className="browser-default add-sale-box">
-              { this.employeeOptions() }
-            </select>
-            <br />
+            <div className='col s12'>
+              <label>Select a salesman</label>
+              <select ref='employee' className="browser-default add-sale-box">
+                { this.employeeOptions() }
+              </select>
+              <br />
+            </div>
             <div className='col s12 m5' >
               <p style={styles.customLabel}>Customer First Name</p>
               <input type='text' ref='firstName' className="add-sale-box" required placeholder="First Name" />
@@ -147,28 +149,30 @@ employeeFind(id) {
               <p style={styles.customLabel}>Customer Last Name</p>
               <input type='text' ref='lastName' className="add-sale-box" required placeholder="Last Name" />
             </div>
-            <div className='col s12 m2'>              
+            <div className='col s12 m2'>
               <p style={styles.customLabel}>KW</p>
               <input type='text' ref='kw' className="add-sale-box" required placeholder="ex. 5.2"/>
             </div>
-            <p>
+            <p className='col s12 l3 center'>
               <input type="checkbox" id="sitdown" className='filled-in checkbox-blue add-sale-box' />
               <label htmlFor="sitdown">Sit Down</label>
             </p>
-            <p>
+            <p className='col s12 l3 center'>
               <input type="checkbox" id="close" className='filled-in checkbox-blue' />
               <label htmlFor="close">Closed Sale</label>
             </p>
-            <p>
+            <p className='col s12 l3 center'>
               <input type="checkbox" id="sitesurvey" className='filled-in checkbox-blue' />
               <label htmlFor="sitesurvey">Site Survey</label>
             </p>
-            <p>
+            <p className='col s12 l3 center'>
               <input type="checkbox" id="cancel" className='filled-in checkbox-blue' />
               <label htmlFor="cancel">Cancelled</label>
             </p>
-            <label>Date</label>
-            <input type="date" ref='date' className="datepicker add-sale-box" placeholder='click to select date' />
+            <div className='col s12'>
+              <label>Date</label>
+              <input type="date" ref='date' className="datepicker add-sale-box" placeholder='click to select date' />
+            </div>
           </div>
           <div className='col s6 offset-s3 l4 offset-l4' style={{marginBottom: '30px', marginTop: '10px'}}>
             <button type='submit' className='btn' style={{backgroundColor: '#60b9e8', width: '100%', textAlign: 'center'}}>Add Sale</button>
