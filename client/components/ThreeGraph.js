b432ae627294eac549b4659ae6a38b61d0459f34
Josh Terry <joshterry55@gmail.com>
2017-03-10 11:50:36 -0700

    fixed graph color for safari and firefox users

--- client/components/ThreeGraph.js
+++ client/components/ThreeGraph.js
@@ -34,22 +34,8 @@ class ThreeGraph extends React.Component {
           datasets: [{
               label: '# of Site Surveys',
               data: [sixMonthKw[sixMonths[0]], sixMonthKw[sixMonths[1]], sixMonthKw[sixMonths[2]], sixMonthKw[sixMonths[3]], sixMonthKw[sixMonths[4]], sixMonthKw[sixMonths[5]]],
-              backgroundColor: [
-                'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                'rgba(54, 162, 235, 1)',
-                  'rgba(255,99,132,1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderColor: 'rgba(54, 162, 235, 1)',
               borderWidth: 1
           }]
       },
@@ -93,22 +79,8 @@ class ThreeGraph extends React.Component {
           datasets: [{
               label: '# of Site Surveys',
               data: [sixMonthKw[sixMonths[0]], sixMonthKw[sixMonths[1]], sixMonthKw[sixMonths[2]], sixMonthKw[sixMonths[3]], sixMonthKw[sixMonths[4]], sixMonthKw[sixMonths[5]]],
-              backgroundColor: [
-                'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                'rgba(54, 162, 235, 1)',
-                  'rgba(255,99,132,1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderColor: 'rgba(54, 162, 235, 1)',
               borderWidth: 1
           }]
       },
