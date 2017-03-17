d3e01b6b67739c4d7f9ab92a12038a2baad2d10e
Josh Terry <joshterry55@gmail.com>
2017-03-16 19:27:54 -0600

    company can change color

--- client/components/Settings.js
+++ client/components/Settings.js
@@ -8,6 +8,8 @@ class Settings extends React.Component {
     super(props)
 
     this.state = {edit: false}
+
+    this.updateCompany = this.updateCompany.bind(this)
   }
 
   componentDidMount() {
@@ -16,11 +18,33 @@ class Settings extends React.Component {
     // }
   }
 
+  updateCompany(e) {
+    e.preventDefault()
+    let truePerc = this.refs.truePercentage.value
+    let cancelPerc = this.refs.cancelPercentage.value
+    let id = this.props.assignedcompany.id
+    let color = this.refs.color.value
+    $.ajax({
+      url: `/api/companies/${id}`,
+      type: 'PUT',
+      dataType: 'JSON',
+      data: { company: {
+        true_percentage: truePerc,
+        cancel_percentage: cancelPerc,
+        color: color
+      }}
+    }).done( company => {
+      this.props.dispatch({type: 'ASSIGNED_COMPANY', company})
+    }).fail( data => {
+
+    })
+  }
+
   display() {
     let company = this.props.assignedcompany
     return(
       <div>
-        <form className='col s12 test-test' style={{backgroundColor: '#f27f7', minHeight: '500px', fontSize: '13px'}} onSubmit={this.updateEmployee}>
+        <form className='col s12 test-test' style={{backgroundColor: '#f27f7', minHeight: '500px', fontSize: '13px'}} onSubmit={this.updateCompany}>
           <div className='col s12 m6 l4' style={{marginBottom: '30px', height: '40px'}}><b>Closing percentage above:</b>
             <input type='text' ref='truePercentage' style={{backgroundColor: 'white'}} className='employee-info' defaultValue={company.true_percentage} required/>
           </div>
@@ -28,15 +52,14 @@ class Settings extends React.Component {
             <input type='text' ref='cancelPercentage' style={{backgroundColor: 'white'}} className='employee-info' defaultValue={company.cancel_percentage} required/>
           </div>
           <div className='col s12 m6 l4' style={{marginBottom: '30px', height: '40px', paddingRight: '0px'}}><b>Company Color (for navbar)</b> <br />
-            <select ref='role' className="browser-default employee-info">
-              <option value='#354458'><span style={{backgroundColor: '#354458'}}>Navy Blue</span></option>
-              <option value='#0C98CF'><span style={{backgroundColor: '#0C98CF'}}>Royal Blue</span></option>
+            <select ref='color' className="browser-default employee-info" defaultValue={company.color}>
+              <option value='#354458'>Navy Blue</option>
+              <option value='#0C98CF'>Royal Blue</option>
             </select>
           </div>
           <div className='col s12'> </div>
           <div className='col s12'>
             <input type='submit' className='btn' style={{backgroundColor: '#60b9e8'}} value='Update'/> &nbsp;
-            <span className='btn' style={{backgroundColor: '#F53D3D'}}>Delete</span>
           </div>
         </form>
       </div>
