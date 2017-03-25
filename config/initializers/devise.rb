cf4650b9599766d2d26a0e5e4e58c53bd51edf41
Josh Terry <joshterry55@gmail.com>
2017-03-24 22:54:29 -0600

    another test

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,8 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.allow_insecure_token_lookup = true
-  config.mailer_sender = 'solarsalesmend@gmail.com'
+
+  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
