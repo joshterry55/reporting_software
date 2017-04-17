b1e6a2db7e7f7d5f729a590044a6750538193640
Rinku <rinkukushwah1008@gmail.com>
2017-04-17 12:10:38 +0530

    chnages for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(:email=>"test@mailinator.com", :password=>"12345678", :first_name=>"test", :last_name=>"test")
